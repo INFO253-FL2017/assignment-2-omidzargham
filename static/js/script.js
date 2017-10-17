1949d4ecf363099bd5bc8e6fedecedc69e423e47
Omid Zargham <ozargham@berkeley.edu>
2017-10-16 23:58:54 -0700

    Finished

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,33 @@
-// script.js
+var inputName = document.getElementById('inputName');
+var subject = document.getElementById('subject');
+var message = document.getElementById('message');
+var form = document.getElementById('form');
+var messageArea = document.getElementById('messageArea');
 
-console.log("js/script.js has been loaded!");
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+	if (subject.value != "" && inputName.value != "" && message.value != "") {
+		messageArea.innerHTML = "Your message has been sent";
+	}
+	else {
+		messageArea.innerHTML = "Missing fields: " + handleSubmitHelper();
+	}
+	//event.preventDefault();
+}
+
+function handleSubmitHelper() {
+	finalString = ""
+	if (inputName.value == "") {
+		finalString += "name, ";
+	}
+	if (subject.value == "") {
+		finalString += "subject, ";
+	}
+	if (message.value == "") {
+		finalString += "message, ";
+	}
+	finalString = finalString.slice(0, -2)
+	return finalString
+}
+
--- webserver.py
+++ webserver.py
@@ -1,19 +1,55 @@
-"""
-webserver.py
-
-File that is the central location of code for your webserver.
-"""
-
-from flask import Flask, render_template
-
-# Create application, and point static path (where static resources like images, css, and js files are stored) to the
-# "static folder"
+from flask import Flask, request, render_template
 app = Flask(__name__,static_url_path="/static")
+import requests
+import os
 
 @app.route('/')
-def hello_world():
-    """
-    If someone goes to the root of your website (i.e. http://localhost:5000/), run this function. The string that this
-    returns will be sent to the browser
-    """
-    return render_template("index.html") # Render the template located in "templates/index.html"
+@app.route('/index')
+def root():
+	"""
+	If someone goes to the root of your website (i.e. http://localhost:5000/), run this function. The string that this
+	returns will be sent to the browser
+	"""
+	return render_template("index.html") # Render the template located in "templates/index.html"
+
+@app.route('/about')
+def about():
+	return render_template("about_us.html")
+
+@app.route('/contact', methods=['GET'])
+def contact():
+	return render_template("contact_us.html")
+
+@app.route('/contact/hi', methods=['POST'])
+def contact_post():
+	email_name = request.form.get('inputName')
+	email_subject = request.form.get('subject')
+	email_message = request.form.get('message')
+	api_base_url = "https://api.mailgun.net/v3/"
+
+	auth = (os.environ["INFO253_MAILGUN_USER"], os.environ["INFO253_MAILGUN_PASSWORD"])
+	data = {
+		'from': email_name + " <" + os.environ["INFO253_MAILGUN_FROM_EMAIL"] + ">",
+		'to': os.environ["INFO253_MAILGUN_TO_EMAIL"],
+		'subject': email_subject,
+		'text': email_message}
+
+	print(data)
+	print(auth)
+
+	send_message =  requests.post(
+		api_base_url + os.environ["INFO253_MAILGUN_DOMAIN"] + "/messages", 
+		auth=auth,
+		data=data)
+	print(api_base_url + os.environ["INFO253_MAILGUN_DOMAIN"] + "/messages")
+	if send_message.status_code == requests.codes.ok:
+		add_to_body = "Hi" + email_name + "Your message has been sent!"
+	else:
+		add_to_body = "An error occurred. Your message was unable to send :("
+
+	return render_template("contact_us.html", add_to_body=add_to_body)
+
+@app.route('/blog/<post_name>')
+def blog_posts(post_name):
+	return render_template("raw_content/" + post_name + ".html")
+  
